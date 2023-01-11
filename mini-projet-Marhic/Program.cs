aedc96f59d1e3f17a026c86c5e4298e15a170a80
RAPHAËL RAULT <githubuser@mailo.com>
2023-01-11 17:04:48 +0100

    modification de liste vers string

--- mini-projet-Marhic/Program.cs
+++ mini-projet-Marhic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Xml.Linq;
 
 class miniProjetMarhic
@@ -8,9 +9,9 @@ class miniProjetMarhic
     /// </summary>
     /// <param name="pile">Liste de string, représentant une pile.</param>
     /// <param name="valeur">valeur à ajouter dans la pile.</param>
-    public static void push(List<string>pile, string valeur)
+    public static void Push(ref string pile, string valeur)
     {
-        if (pile.Count()==0)
+        if (pile == null)
         {
             throw new ArgumentNullException("l'élément est nul.");
         }
@@ -18,7 +19,7 @@ class miniProjetMarhic
         {
             throw new ArgumentNullException("la pile est nulle.");
         }
-        pile.Add(valeur);
+        pile+=valeur;
     }
 
     /// <summary>
@@ -26,13 +27,14 @@ class miniProjetMarhic
     /// </summary>
     /// <param name="pile">Liste de string, représentant une pile.</param>
     /// <returns>retourne la dernière valeur de la liste.</returns>
-    public static string top(List<string> pile)
+    public static string Top(string pile)
     {
-        if (pile.Count()==0)
+        if (pile == "")
         {
             throw new ArgumentNullException("La pille est nulle.");
         }
-        return pile[pile.Count() - 1];
+        
+        return pile[pile.Length-1].ToString();
 
     }
 
@@ -41,38 +43,55 @@ class miniProjetMarhic
     /// </summary>
     /// <param name="pile">La liste de string, représentant une pile.</param>
     /// <returns>retourne la valeur supprimée.</returns>
-    public static string pop(List<string> pile)
+    public static string Pop(ref string pile)
     {
-        if (pile.Count()==0)
+        string dernierElement = Top(pile);
+        string pileConcatenee = "";
+        if (pile == "")
         {
             throw new ArgumentNullException("La pile est nulle.");
         }
-        string val = top(pile);
-        pile.RemoveAt(pile.Count() - 1); //Suppression du dernier élément de la liste.
-        return val;
+        
+        for (int i = 0;i<=pile.Length-2;i++)
+        {//Suppression du dernier élément de la liste.
+            pileConcatenee+= pile[i].ToString();
+        }
+        pile = pileConcatenee;
+        return dernierElement;
+    }
 
+    static string Split(string expressionNonFormatee)
+    {
+        string expression = "";
+        foreach (char element in expressionNonFormatee)
+        {
+            if (element != ' ')
+            {
+                expression += element;
+            }
+        }
+        return expression;
     }
 
     /// <summary>
     /// Récupère le calcul de l'utilisateur (input)
     /// </summary>
     /// <returns>retourne l'expression entrée.</returns>
-    public static List<string> getCalcul()
+    public static string getCalcul()
     { 
-        List<string> expression = new List<string>(); //Déclaration d'une nouvelle liste qui contiendra l'expression
+        string expression = ""; //Déclaration d'une nouvelle liste qui contiendra l'expression
         Console.WriteLine("Entrez le calcul : \n");
         string calcul = Console.ReadLine(); //Entrée utilisateur.
         if (calcul == null)
         {
             throw new ArgumentNullException("le calcul entré est nul");
         }
-
-    List<String> elementExpression = calcul.Split(' ').ToList(); //Enlève les espaces de l'expression.
-
-        for(int i = 0; i < elementExpression.Count; i++) //parcours l'expression.
-        {
-            push(expression, elementExpression[i]);// ajout de chaque élément de l'expression à une 
-        }
+        
+        expression += Split(calcul); //Enlève les espaces de l'expression.
+        //for(int i = 0; i < calcul.Length-1; i++) //parcours l'expression.
+        //{
+        //    push(expression, calcul[i]);// ajout de chaque élément de l'expression à une 
+        //}
         return expression;
     }
 
@@ -83,10 +102,10 @@ class miniProjetMarhic
     /// <param name="deuxiemeValeur">deuxieme valeur de l'expression</param>
     /// <param name="pile">pile contenant les nombres de l'expression</param>
     /// <exception cref="Exception"></exception>
-    public static void affectationEtVerification(ref float premiereValeur, ref float deuxiemeValeur, List<string> pile)
+    public static void affectationEtVerification(ref float premiereValeur, ref float deuxiemeValeur,ref string pile)
     {
-        deuxiemeValeur = float.Parse(pop(pile));//la valeur en haut de la pile devient la deuxième valeur de l'opération
-        premiereValeur = float.Parse(pop(pile));
+        deuxiemeValeur = float.Parse(Pop(ref pile));//la valeur en haut de la pile devient la deuxième valeur de l'opération
+        premiereValeur = float.Parse(Pop(ref pile));
         if (premiereValeur < 0 || deuxiemeValeur < 0)//vérifie qu'aucune valeur n'est négative
         { 
             throw new Exception("Erreur : Valeurs négatives interdites.");
@@ -98,31 +117,32 @@ class miniProjetMarhic
     /// </summary>
     /// <param name="expression">Expression entrée par l'utilisateur.</param>
     /// <returns>Retourne la somme finale.</returns>
-    public static float Calcul(List<string> expression)
+    public static float Calcul(string expression)
     {
-        List<string> pile = new List<string>();
+        string pile = "";
         float somme = 0;
         float premiereValeur = 0;
         float deuxiemeValeur = 0;
-
-        foreach (string element in expression) //Parcours tous les éléments de l'expression
+        foreach (char element in expression) //Parcours tous les éléments de l'expression
         {
             try //Utilisé pour throw une exception si il y a une erreur.
             {
                 switch (element) //Utilisé pour chaque opérateur.
                 {
-                    case "+": //gestion de l'addition.
-                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur, pile);
+                    
+                    case '+': //gestion de l'addition.
+                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur,ref pile);
+           
                         somme = premiereValeur + deuxiemeValeur;
-                        push(pile, somme.ToString());
+                        Push(ref pile, somme.ToString());
                         break;
-                    case "-":
-                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur, pile);
+                    case '-':
+                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur, ref pile);
                         somme = premiereValeur - deuxiemeValeur;
-                        push(pile, somme.ToString());
+                        Push(ref pile, somme.ToString());
                         break;
-                    case "/":
-                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur, pile);
+                    case '/':
+                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur,ref pile);
                         if(premiereValeur == 0)
                         {
                             throw new DivideByZeroException("Valeurs entrées incorrectes : division par zéro");
@@ -130,16 +150,16 @@ class miniProjetMarhic
                         else
                         {
                             somme = premiereValeur / deuxiemeValeur;
-                            push(pile, somme.ToString());
+                            Push(ref pile, somme.ToString());
                         }
                         break;
-                    case "*":
-                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur, pile);
+                    case '*':
+                        affectationEtVerification(ref deuxiemeValeur, ref premiereValeur,ref pile);
                         somme = premiereValeur * deuxiemeValeur;
-                        push(pile, somme.ToString());
+                        Push(ref pile, somme.ToString());
                         break;
-                    default:
-                        push(pile, element);
+                    default: //si l'element n'est pas un opérateur, c'est donc un nombre, on l'ajoute à la pile.
+                        Push(ref pile, element.ToString());
                         break;
                 }
             }
@@ -158,7 +178,6 @@ class miniProjetMarhic
     static void Main()
     {
         bool programRunning = true;
-
         do
         {
             Console.WriteLine( Calcul(getCalcul()));
